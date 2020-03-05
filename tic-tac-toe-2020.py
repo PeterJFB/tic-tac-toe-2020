549d92ae9208f0f4ba7d0ac0c4fbf738550d8377
Peter Johan Flått-Bjørnstad <peterj.flbj@gmail.com>
2020-03-05 20:33:06 +0100

    some comments, structure, and ocd

--- tic-tac-toe-2020.py
+++ tic-tac-toe-2020.py
@@ -1,38 +1,49 @@
 import pygame as pg
+
 pg.init()
 
-# Board
-width = 3
-height = 5
-win_length = 3
+# Board properties
+width = 7
+height = 7
+win_length = 4
+
+print(f"Win length is {win_length} tiles\nwidth {width} tiles\nheight {height} tiles")
+
 board = [width * [0] for h in range(height)]
 spots = width * height
+player = 1
 
 # Window
-win_width, win_height = 1280, 720
+win_width, win_height = 1280, 720  # Starting resolution
 win_info = pg.display.Info()
 
-# Info
+# Info details
 bar = [260, 50]  # px
 font = pg.font.SysFont('trebuchetms', 40)
 bar_text = font.render("Player 1's turn", False, (255, 255, 255))
 bar_text_width, bar_text_height = bar_text.get_rect()[2:4]
 
-# Tile
+# Tile creation
 tile_color = {0: (255, 255, 255), 1: (255, 0, 0), -1: (0, 0, 255)}
 tile_size = win_width // width if min([win_width/width, (win_height - bar[1])/height]) == win_width/width \
                 else (win_height - bar[1]) // height
 tile_margin = round(tile_size * 0.1) if round(tile_size * 0.1) > 1 else 1
 tile_size = round(tile_size * 0.9) if round(tile_size * 0.9) > 1 else 1
 
+hover = []
+hover_tile = pg.Surface((tile_size, tile_size))
+hover_tile.set_alpha(128)
+hover_tile.fill(tile_color[player])
+
 # Window again
 win_width = tile_size * width + tile_margin * (width + 1)
 win_width = bar[0] if bar[0] > win_width else win_width
 win_height = tile_size * height + tile_margin * (height + 1) + bar[1]
-print(win_width, win_height)
 win = pg.display.set_mode([win_width, win_height], pg.RESIZABLE)
+resized = False
 
 
+# Algorithms to detect if a player has won
 def horizontal(p):
     for y in range(height):
         count = 0
@@ -89,7 +100,7 @@ def fullTest(p):
     if any([horizontal(p), vertical(p), bslash(p), slash(p)]):
         return True
 
-
+# Drawing tiles and text
 def draw():
     win.fill((0, 0, 0))
     # generate tiles
@@ -104,19 +115,14 @@ def draw():
         win.blit(hover_tile, (tile_margin + hover[1] * (tile_margin + tile_size),
                               tile_margin + hover[0] * (tile_margin + tile_size) + bar[1]))
 
-    # Apply text
+    # Draw text
     win.blit(bar_text, ((win_width - bar_text_width)//2, (bar[1] - bar_text_height)//2))
 
 
 # Setup
-player = 1
-hover = []
-hover_tile = pg.Surface((tile_size, tile_size))
-hover_tile.set_alpha(128)
-hover_tile.fill(tile_color[player])
-resized = False
 game = True
 end = False
+# Main
 while not end:
     if game:
         # Interaction
@@ -160,7 +166,6 @@ while not end:
             else:
                 hover = []
 
-
     # Other property updates
     for event in pg.event.get():
 
@@ -172,7 +177,6 @@ while not end:
         elif event.type == pg.VIDEORESIZE:
             if not resized:
                 win_width, win_height = event.size
-                print(event.size)
                 tile_size = win_width // width if min([win_width/width, (win_height - bar[1])/height]) == win_width/width \
                     else (win_height - bar[1]) // height
                 tile_margin = round(tile_size * 0.1) if round(tile_size * 0.1) > 1 else 1
@@ -183,16 +187,13 @@ while not end:
                 win_width = tile_size * width + tile_margin * (width + 1)
                 win_width = bar[0] if bar[0] > win_width else win_width
                 win_height = tile_size * height + tile_margin * (height + 1) + bar[1]
-                print(win_width, win_height)
                 win = pg.display.set_mode([win_width, win_height], pg.RESIZABLE)
                 resized = True
             else:
                 resized = False
 
-
     # Update screen
     draw()
     pg.display.update()
 
-
-pg.quit()
+pg.quit()  # Perfect.
